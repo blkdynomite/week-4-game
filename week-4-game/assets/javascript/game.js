7f4ca5bac42d54be66b3a0356fe8410c4565e722
blkdynomite <jst9259@gmail.com>
2017-03-16 14:03:35 -0400

    Add files via upload
    3rd pass at Crystal game

---
+++ week-4-game/assets/javascript/game.js
@@ -0,0 +1,24 @@
+// Declaring variables here
+
+var crystal1 = "button.click";
+var crystal2 = "button.click";
+var crystal3 = "button.click";
+var crystal4 = "button.click";
+
+
+// attempted first stages of interactivity
+function button.crystal1() {
+	// body...
+}
+
+function button.crystal2() {
+	// body...
+}
+
+function button.crystal3() {
+	// body...
+}
+
+function button.crystal4() {
+	// body...
+}
